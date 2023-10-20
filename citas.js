b113b8da47df03605ca70965150369c4f7e2e0e0
Nicolas Vallejo <vallejofernandonicolas@gmail.com>
2023-10-20 16:32:15 -0300

    primer commit

---
+++ citas.js
@@ -0,0 +1,43 @@
+let citas = [
+  {
+    autor: "albert Einstein",
+    texto: "No poseo talentos especiales. solo soy apasionadamente curioso.",
+  },
+  {
+    autor: "Lao Tse",
+    texto: "No hay que ir para atrás ni para darse impulso.",
+  },
+  {
+    autor: "John Lennon",
+    texto: "Haz el amor y no la guerra.",
+  },
+  {
+    autor: "Charles Baudelaire",
+    texto:
+      "Para trabajar basta estar convencido de una cosa: que trabajar es menos aburrido que divertirse.",
+  },
+  {
+    autor: " Steve Jobs",
+    texto: "El único modo de hacer un gran trabajo es amar lo que haces.",
+  },
+  {
+    autor: "Nelson Mandela",
+    texto:
+      "El dinero no es la clave del éxito; la libertad para poder crear lo es.",
+  },
+  {
+    autor: "Aristóteles",
+    texto:
+      "La inteligencia consiste no sólo en el conocimiento, sino también en la destreza de aplicar los conocimientos en la práctica.",
+  },
+  {
+    autor: "Colin Powell",
+    texto:
+      "Un sueño no se hace realidad por arte de magia, necesita sudor, determinación y trabajo duro.",
+  },
+  {
+    autor: "Bill Cosby",
+    texto:
+      "Para tener éxito tu deseo de alcanzarlo debe ser mayor que tu miedo al fracaso.",
+  },
+];
